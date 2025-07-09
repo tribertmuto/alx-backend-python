625c015bdc0ede84cb6b8599a8b42a24a0c18160
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-09 16:26:35 +0200

    Update 0-log_queries.py

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,14 +1,9 @@
 import sqlite3
 import functools
 
-def log_queries(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        query = kwargs.get('query') or (args[0] if args else None)
-        if query:
-            print(f"[LOG] Executing SQL Query: {query}")
-        return func(*args, **kwargs)
-    return wrapper
+#### decorator to lof SQL queries
+
+ """ YOUR CODE GOES HERE"""
 
 @log_queries
 def fetch_all_users(query):
@@ -19,5 +14,5 @@ def fetch_all_users(query):
     conn.close()
     return results
 
+#### fetch users while logging the query
 users = fetch_all_users(query="SELECT * FROM users")
-print(users)
