a87b935cd27c836579be58172a25ffbbc12f1b9b
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-08-10 22:03:03 +0200

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,25 +1,21 @@
-import sqlite3
-import functools
-from datetime import datetime  # ✅ Required for timestamp logging
+(venv)faithokoth@Faiths-MacBook-Pro python-generators-0x00 % cat 1-main.py 
 
-def log_queries(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        query = kwargs.get('query') or (args[0] if args else None)
-        if query:
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"[{timestamp}] Executing SQL Query: {query}")
-        return func(*args, **kwargs)
-    return wrapper
+#!/usr/bin/python3
+from itertools import islice
+stream_users = __import__('0-stream_users')
 
-@log_queries
-def fetch_all_users(query):
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
-    conn.close()
-    return results
+# iterate over the generator function and print only the first 6 rows
 
-users = fetch_all_users(query="SELECT * FROM users")
-print(users)
+for user in islice(stream_users(), 6):
+    print(user)
+
+(venv) faithokoth@Faiths-MacBook-Pro python-generators-0x00 %./1-main.py
+
+{'user_id': '00234e50-34eb-4ce2-94ec-26e3fa749796', 'name': 'Dan Altenwerth Jr.', 'email': 'Molly59@gmail.com', 'age': 67}
+{'user_id': '006bfede-724d-4cdd-a2a6-59700f40d0da', 'name': 'Glenda Wisozk', 'email': 'Miriam21@gmail.com', 'age': 119}
+{'user_id': '006e1f7f-90c2-45ad-8c1d-1275d594cc88', 'name': 'Daniel Fahey IV', 'email': 'Delia.Lesch11@hotmail.com', 'age': 49}
+{'user_id': '00af05c9-0a86-419e-8c2d-5fb7e899ae1c', 'name': 'Ronnie Bechtelar', 'email': 'Sandra19@yahoo.com', 'age': 22}
+{'user_id': '00cc08cc-62f4-4da1-b8e4-f5d9ef5dbbd4', 'name': 'Alma Bechtelar', 'email': 'Shelly_Balistreri22@hotmail.com', 'age': 102}
+{'user_id': '01187f09-72be-4924-8a2d-150645dcadad', 'name': 'Jonathon Jones', 'email': 'Jody.Quigley-Ziemann33@yahoo.com', 'age': 116}
+
+(venv) faithokoth@Faiths-MacBook-Pro python-generators-0x00 %
